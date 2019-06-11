2545d9a90d15bd71f92dddb525badbe6614d24f9
Luis Hernández <luis.hlopez@outlook.com>
2019-06-11 12:06:28 -0500

    Add component to be rendered

--- day-1/src/app/app.component.ts
+++ day-1/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { RegistrationData } from './models';
 
 @Component({
   selector: 'app-root',
-  template: ``,
-  styleUrls: ['./app.component.scss']
+  template: `
+    <app-registration-form (registration)="registerUser($event)"></app-registration-form>
+  `,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ng-twitter';
+
+  registerUser(userData: RegistrationData): void {
+    console.log(userData);
+  }
 }
