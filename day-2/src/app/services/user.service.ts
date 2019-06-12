a6144f481223a3890ac24786d8d7a42c91476a54
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-12 08:49:01 -0500

    Add current user getter

--- day-2/src/app/services/user.service.ts
+++ day-2/src/app/services/user.service.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
 
 import { environment } from '@env/environment';
-import { RegistrationData } from '@app/models';
+import { RegistrationData, User } from '@app/models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient) {}
+  get currentUser(): User {
+    const decodedToken = this.jwt.decodeToken(this.jwt.tokenGetter());
+    const user = new User(decodedToken.name, decodedToken.email);
+
+    return user;
+  }
+
+  constructor(private http: HttpClient, private jwt: JwtHelperService) {}
 
   register(data: RegistrationData): Observable<null> {
     return this.http.post<null>(`${environment.apiUrl}/user/create`, data);
-  }
-
-  login(email: string, password: string): Observable<string> {
-    return this.http.post<string>(`${environment.apiUrl}/auth`, { email, password }).pipe(
-      tap((data: any) => localStorage.setItem('token', data.token)),
-      map((data: any) => data.token)
-    );
   }
 }
