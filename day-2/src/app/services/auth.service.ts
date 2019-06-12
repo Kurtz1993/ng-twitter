f1fad3cc569d067439863880398bef1393db120e
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-12 08:49:05 -0500

    Add auth service

---
+++ day-2/src/app/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+
+import { environment } from '@env/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  get isLoggedIn() {
+    return !!localStorage.getItem('token');
+  }
+
+  constructor(private http: HttpClient, private jwt: JwtHelperService) {}
+
+  login(email: string, password: string): Observable<null> {
+    return this.http
+      .post<{ token: string }>(`${environment.apiUrl}/auth`, { email, password })
+      .pipe(
+        tap((data: { token: string }) => localStorage.setItem('token', data.token)),
+        map(() => null)
+      );
+  }
+
+  logout() {
+    localStorage.clear();
+  }
+}
