f8008e89fa04b2fb42aa91f41dae9a48d9dde5b8
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-13 08:19:51 -0500

    Use toasts for errors

--- src/app/services/error-handler.service.ts
+++ src/app/services/error-handler.service.ts
@@ -1,15 +1,27 @@
 import { Injectable, ErrorHandler } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 
+import { NgxSimpleToastsService } from '@kurtz1993/ngx-toasts';
+
 @Injectable()
 export class ErrorHandlerService implements ErrorHandler {
+  constructor(private toastsService: NgxSimpleToastsService) {}
+
   handleError(err: any): void {
     // alert(err.message);
 
     if (err instanceof HttpErrorResponse) {
       console.log('http error...');
       console.log(err.error);
+      this.toastsService.showToast({
+        message: err.error.message,
+        type: 'error',
+      });
       return;
     }
+    this.toastsService.showToast({
+      message: err.message,
+      type: 'error',
+    });
   }
 }
